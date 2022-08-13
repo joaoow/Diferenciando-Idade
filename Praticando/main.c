8b3f5d13a12b57abdae48130a36e8a35d1ed62f3
Joaoow <joaomotionless570@gmail.com>
2022-08-13 18:01:25 -0300

    Add files via upload

---
+++ Praticando/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    // Autor: João
+
+    int idade;
+
+
+            printf("Digite sua idade: \n");
+            scanf("%i", &idade);
+
+            if(idade >= 18){
+            printf("Maior de idade!\n");
+            printf("Pode entrar na balada.\n");
+    }
+
+    if(idade <= 18){
+        printf("Desculpe,voce ainda e menor de idade!\n");
+        printf("Nao pode entrar na balada\n");
+    }
+
+
+
+    return 0;
+}
